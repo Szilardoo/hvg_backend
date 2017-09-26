5b4f3d106e50ae1b37411f456ceaca5818e98a33
Dóczi Szilárd <doczi.szilard@axis.hu>
2017-09-26 12:20:44 +0200

    new version

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.put('/mail', function (req, res) {
 	});
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("Server is running on port: 3000");
 });
